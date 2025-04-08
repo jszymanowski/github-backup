6cfcd47e25ac0e3573951deb6a283d5adb154238
John Szymanowski <github@john.szymanow.ski>
2025-04-08 10:17:09 +0800

    Final

--- backup.sh
+++ backup.sh
@@ -24,6 +24,7 @@ do
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting backup..."
     echo ""
     uv run github-backup.py /home/docker/github-backup/config/config.json
+    echo ""
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] Backup completed. Sleeping for $SCHEDULE seconds..."
     echo ""
     echo "----------------------------------------------------------------------------------"
