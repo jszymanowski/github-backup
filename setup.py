1a33dc227f7d6b1783e2e8d04bf0a374f9103de6
John Szymanowski <github@john.szymanow.ski>
2025-04-08 08:43:33 +0800

    Keep zip_safe

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name="github-backup",
-    version="1.1.0",
+    version="1.0.1",
     description="",
     url="https://github.com/jszymanowski/github-backup",
     author="John Szymanowski, lnxd",
     install_requires=["requests"],
     scripts=["github-backup.py"],
+    zip_safe=True,
 )
