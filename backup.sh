b779c09de764f1e4af3730a377c7da78f38f1b59
John Szymanowski <github@john.szymanow.ski>
2025-04-08 09:22:33 +0800

    Fix: Remove invalid TZ specification

--- backup.sh
+++ backup.sh
@@ -23,8 +23,8 @@ cp /home/docker/github-backup/config.json /home/docker/github-backup/config/conf
 # Start backup
 while true
 do
-    echo "[$(date '+%Y-%m-%d %H:%M:%S' --date='TZ="`cat /etc/timezone`"')] Starting backup..."
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting backup..."
     python3 github-backup.py /home/docker/github-backup/config/config.json
-    echo "[$(date '+%Y-%m-%d %H:%M:%S' --date='TZ="`cat /etc/timezone`"')] Backup completed. Sleeping for $SCHEDULE seconds..."
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Backup completed. Sleeping for $SCHEDULE seconds..."
     sleep $SCHEDULE
 done
