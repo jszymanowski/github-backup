f9cdf8979bb3aa8b206d8139e288da921e77db0a
John Szymanowski <github@john.szymanow.ski>
2025-04-08 08:09:06 +0800

    Improve logging (#4)

--- backup.sh
+++ backup.sh
@@ -22,7 +22,9 @@ cp /home/docker/github-backup/config.json /home/docker/github-backup/config/conf
 
 # Start backup
 while true
- do python3 github-backup.py /home/docker/github-backup/config/config.json
- chown -R nobody /home/docker/backups
- sleep $SCHEDULE
+do
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting backup..."
+    python3 github-backup.py /home/docker/github-backup/config/config.json
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Backup completed. Sleeping for $SCHEDULE seconds..."
+    sleep $SCHEDULE
 done
