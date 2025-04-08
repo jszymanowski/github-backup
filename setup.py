ef5b23add6cecad501bd417062e5e1016cfc9271
John Szymanowski <github@john.szymanow.ski>
2025-04-08 08:35:11 +0800

    Keep setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="github-backup",
+    version="1.1.0",
+    description="",
+    url="https://github.com/jszymanowski/github-backup",
+    author="John Szymanowski",
+    install_requires=["requests"],
+    scripts=["github-backup.py"],
+)
